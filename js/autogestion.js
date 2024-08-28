f1ce818668d41e4b357ce2e2ba02e6e2752d3acc
Daniela Iuorio <lic.iuorio.daniela@gmail.com>
2024-08-28 00:00:14 -0300

    html y css Autogestion

---
+++ js/autogestion.js
@@ -0,0 +1,9 @@
+
+
+{/* <form action="" method="post" class="user-form" id="user-personal-data">
+<input type="text" id="user-username" placeholder="Nombre de Usuario">
+<input type="text" id="user-name-surname" placeholder="Nombre y Apellido">
+<input type="email" name="email" id="user-email" placeholder="name-and-surname@mail.com">
+<input type="tel" name="mobile-phone" id="user-mobile-phone" placeholder="+54 9 11 1234 5678">
+    </form>
+    <button id="guardar-form">Guardar</button> */}
