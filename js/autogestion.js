07d4b7b28a667918a976b80cfe779e5267a28401
Daniela Iuorio <lic.iuorio.daniela@gmail.com>
2024-08-30 15:24:58 -0300

    funciones autogestion

--- js/autogestion.js
+++ js/autogestion.js
@@ -1,7 +1,8 @@
+// Recupero datos de curso elegido y usuario
 let cursoElegido = JSON.parse(localStorage.getItem("cursoSeleccionado"))
 let usuarioGestion = JSON.parse(localStorage.getItem("usuario"))
-// console.log(usuarioGestion)
 
+// Muestro toda la info
 function completarDatosIniciales() {
 
     let usuarioWelcome = document.getElementById("user-welcome")
@@ -20,6 +21,9 @@ function completarDatosIniciales() {
     let curso = document.getElementById("nombre-curso")
     curso.innerText = cursoElegido.nombre
 
+    let profe = document.getElementById("profe-curso")
+    profe.innerText = cursoElegido.profe
+
     let cargaHoraria = document.getElementById("info-carga-horaria")
     cargaHoraria.innerText = usuarioGestion.horas
 
@@ -28,31 +32,137 @@ function completarDatosIniciales() {
 
     let primerTotal = document.getElementById("info-total-pagar")
     primerTotal.innerHTML = "$" + usuarioGestion.total
+
 }
 
 completarDatosIniciales()
 
+//Usuario realiza MODIFICACIONES por SECCION (usuario, curso, facturación)
 
+//USUARIO
+// Habilito la modificación de los datos de usuario
 function modificarDatosUsuario() {
     let texto = document.getElementById("user-div-text")
-    texto.innerHTML = `<form class="flex-column" action="" method="post"
-        class="user-form" id="user-personal-data">
+    texto.innerHTML = `<form class="flex-column user-form" action="" method="post"
+        id="user-personal-data">
         <input type="text" id="user-username" placeholder="Nombre de Usuario">
         <input type="text" id="user-name-surname" placeholder="${usuarioGestion.nombre} ${usuarioGestion.apellido}">
         <input type="email" name="email" id="user-email" placeholder="${usuarioGestion.email}">
         <input type="tel" name="mobile-phone" id="user-mobile-phone" placeholder="${usuarioGestion.celular}">
-        </form>
-        <button id="guardar-form">Guardar</button>`
+        <div class="form-item">
+        <input id="botonGuardarUsuario" class="contact-form-item contact_button sin-borde btn btn-outline-success" type="submit" value="Guardar">
+        </div>
+        </form>`
+}
+
+// Ejecuto modificación de datos Usuario
+document.getElementById('modificar-datos-personales').addEventListener('onclick', function (event) {
+    modificarDatosUsuario()
+})
+
+// Guardo info Usuario y muestro
+function guardarInfoUsuario() {
+    localStorage.setItem
+}
+
+
+
+// Ejecuto guardado de datos Usuario
+document.getElementById('botonGuardarUsuario').addEventListener('submit', function (event) {
+    event.preventDefault()
+    guardarInfoUsuario()
+})
+
+//CURSO
+//Habilito la modificación de días de cursada 
+function modificarDiasHorarios() {
+    let horario = document.getElementById("horario")
+    horario.innerHTML = `
+        <form class="flex-column" action="" method="post">
+        <select name="horario" id="horario">
+        <option value="" disabled selected>Seleccione una opción</option>
+        <option value="1">${cursoElegido.dia1}</option>
+        <option value="2">${cursoElegido.dias2}</option>
+        <option value="3">${cursoElegido.dias3}</option>
+        </select>
+        <input id="botonGuardarHorario" class="contact-form-item contact_button sin-borde btn btn-outline-success" type="submit" value="Guardar">
+        </form>`
+    
+}
+
+//Ejecuto modificación de cursada
+document.getElementById('modificar-curso').addEventListener('onclick', function (event) {
+    modificarDiasHorarios()
+})
+
+
+// Guardo info Horario y muestro
+
+function guardarDatosHorario (){
 
 }
 
-let boton = document.getElementById("modificar-datos-personales")
-boton.onclick = () => { modificarDatosUsuario() }
+//Ejecuto guardado de datos Horario
+document.getElementById('botonGuardarHorario').addEventListener('submit', function (event) {
+    event.preventDefault()
+    guardarDatosHorario()
+})
+
+//FACTURACION
+//Habilito modificación de  modalidad de pago
+function modificarPago() {
+    let pago = document.getElementById("pago")
+    pago.innerHTML = `
+        <form class="flex-column" action="" method="post">
+        <select name="horario" id="horario">
+        <option value="" disabled selected>Seleccione una opción</option>
+        <option value="1">Semanal</option>
+        <option value="2">Mensual</option>
+        <option value="3">Trimestral</option>
+        </select>
+        <input id="botonGuardarHorario" class="contact-form-item contact_button sin-borde btn btn-outline-success" type="submit" value="Guardar">
+        </form>`
+    
+}
+
+//Ejecuto modificacion de modalidad de pago
+document.getElementById('modificar-pago').addEventListener('onclick', function (event) {
+    modificarPago()
+})
+
+// Guardo y muestro info Pago
+function guardarDatosPago (){
+
+}
+
+//Ejecuto guardado de datos Pago
+document.getElementById('botonGuardarPago').addEventListener('submit', function (event) {
+    event.preventDefault()
+    guardarDatosPago()
+})
 
 
+// Calculo total a pagar según carga horaria y modalidad de pago
+
+function calcularTotal() {
+    let cargaHoraria = parseInt(document.getElementById('cantidad-horas').value)
+    let formaPago = document.getElementById('forma-pago').value
+    let total = 0
+    switch (formaPago) {
+        case 'semanal':
+            total = (cursoGuardado.precio) * cargaHoraria
+            break
+        case 'mensual':
+            total = (((cursoGuardado.precio) * cargaHoraria) * 4) * 0.9
+            break
+        case 'trimestral':
+            total = ((((cursoGuardado.precio) * cargaHoraria) * 4) * 3) * 0.85
+            break
+        default:
+            total = 0
+    }
+    return total
+}
 
 
-
-
-
-
+// Muestro total en interfaz
